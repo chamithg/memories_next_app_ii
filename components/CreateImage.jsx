dd030ae46427b4f38861685b1e06933e0a0c4e51
gamagegit <cgamage92@yahoo.com>
2023-07-22 02:37:09 -0700

    all validations are in place

--- components/CreateImage.jsx
+++ components/CreateImage.jsx
@@ -8,29 +8,33 @@ const CreateImage = ({ postID, setUploadView }) => {
     image: "",
     caption: "",
   });
+  const [showValidations, setShowValidation] = useState(false);
   const handleSubmit = async (e) => {
     e.preventDefault();
-
-    try {
-      const respsponse = await fetch("/api/profile/album/post/newImage", {
-        method: "POST",
-        body: JSON.stringify({
-          post: postID,
-          caption: uploadImage.caption,
-          image: uploadImage.image,
-        }),
-      });
-
-      if (respsponse.ok) {
-        setUploadImage({
-          image: "",
-          caption: "",
+    if (uploadImage.image && uploadImage.caption) {
+      try {
+        const respsponse = await fetch("/api/profile/album/post/newImage", {
+          method: "POST",
+          body: JSON.stringify({
+            post: postID,
+            caption: uploadImage.caption,
+            image: uploadImage.image,
+          }),
         });
+
+        if (respsponse.ok) {
+          setUploadImage({
+            image: "",
+            caption: "",
+          });
+        }
+      } catch (error) {
+        console.log(error);
+      } finally {
+        setUploadView(false);
       }
-    } catch (error) {
-      console.log(error);
-    } finally {
-      setUploadView(false);
+    } else {
+      setShowValidation(true);
     }
   };
   return (
@@ -47,23 +51,37 @@ const CreateImage = ({ postID, setUploadView }) => {
                 alt="upload_image"
               />
             ) : (
-              <div className="w-32 h-32 border-gray-500 border-2 border-dashed flex flex-col items-center justify-center">
-                <AiOutlineCloudUpload className="w-7 h-7" />
-                <div className="input_file w-24 h-10 overflow-hidden">
-                  <FileBase
-                    type="file"
-                    multiple={false}
-                    onDone={(base64) =>
-                      setUploadImage({ ...uploadImage, image: base64 })
-                    }></FileBase>
+              <>
+                <div>
+                  <div className="w-32 h-32 border-gray-500 border-2 border-dashed flex flex-col items-center justify-center">
+                    <AiOutlineCloudUpload className="w-7 h-7" />
+                    <div className="input_file w-24 h-10 overflow-hidden">
+                      <FileBase
+                        type="file"
+                        multiple={false}
+                        onDone={(base64) =>
+                          setUploadImage({ ...uploadImage, image: base64 })
+                        }></FileBase>
+                    </div>
+                  </div>
                 </div>
-              </div>
+              </>
             )}
           </div>
           <div className="flex flex-col">
             <label className="block text-gray-700 ml-3 text-sm font-bold">
               Caption
             </label>
+            {showValidations && !uploadImage.image && (
+              <h1 className="ml-2 text-red-600 font-semibold font-satoshi">
+                Please choose an image
+              </h1>
+            )}
+            {showValidations && !uploadImage.caption && (
+              <h1 className="ml-2 text-red-600 font-semibold font-satoshi">
+                Please add a caption
+              </h1>
+            )}
             <input
               className="form_input h-3 gap-4 ml-3 max-w-md"
               type="text"
